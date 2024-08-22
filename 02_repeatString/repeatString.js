0079b52a86b87e31e807e963ca443b9948f8c910
kj061404 <kobejordan2004@icloud.com>
2024-08-22 08:48:49 -0500

    I did js exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let counter = 0;
+  string = string;
+  let newString = '';
+
+  if (num < 0) {
+    return 'ERROR';
+  }
+  
+  while (counter < num) {
+
+    newString = newString + string;
+    counter++;
+  }
+
+  return newString;
 
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+ 
