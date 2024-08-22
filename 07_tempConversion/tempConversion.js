dd7bf1f5f02cf5e95dc078c7b68ecd04a30b741c
kj061404 <kobejordan2004@icloud.com>
2024-08-22 15:55:25 -0500

    Did exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let ans = (temp - 32) * (5/9);
+  if (Number.isInteger(ans) === true) {
+    ans = Math.round(ans);
+  }
+  else if (Number.isInteger(ans) === false) {
+    ans = ans.toFixed(1);
+  }
+  return Number(ans);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let ans = (temp * (9/5)) + 32;
+  if (Number.isInteger(ans) === true) {
+    ans = Math.round(ans);
+  }
+  else if (Number.isInteger(ans) === false) {
+    ans = ans.toFixed(1);
+  }
+  return Number(ans);
 };
 
 // Do not edit below this line
