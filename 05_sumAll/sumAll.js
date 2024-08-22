76d58eed486c8c443224d320216c23dbc7fa765d
kj061404 <kobejordan2004@icloud.com>
2024-08-22 15:40:48 -0500

    Did exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,37 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (isNaN(num1)) {
+    return 'ERROR';
+  }
+  else if (isNaN(num2)) {
+    return 'ERROR';
+  }
+  else if (num1 < 0) {
+    return 'ERROR';
+  }
+  else if (num2 < 0) {
+    return 'ERROR';
+  }
+  else if (Number.isInteger(num1) === false) {
+    return 'ERROR';
+  }
+  else if (Number.isInteger(num2) === false) {
+    return 'ERROR';
+  }
+
+  ans = 0;
+  if (num1 < num2) {
+    for (let i = num1; i <= num2; i++) {
+      ans += i;
+  
+    }
+  }
+  else if (num1 > num2) {
+    for (let i = num2; i <= num1; i++) {
+      ans += i;
+  
+    }
+  }
+  return ans;
 
 };
 
