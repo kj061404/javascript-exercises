bb6121a4c73adc3b92a3208a7e07b750b16cce57
kj061404 <kobejordan2004@icloud.com>
2024-08-31 22:36:55 -0500

    I did exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.toLowerCase()
+  string = string.replace(/[^a-zA-Z0-9]/g, '')
+  let newString = string.split('')
+  newString = newString.reverse()
+  newString = newString.join('')
+
+  if (newString === string) {
+    return true;
+  }
+  else {
+    return false;
+  } 
 
 };
 
